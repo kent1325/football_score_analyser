e189c3df8d17df3be1d2f96d91960aaab9f295d1
Kent Vugs Nielsen <19304326+kent1325@users.noreply.github.com>
2020-11-08 16:33:03 +0100

    project funktionality done
    Funktionaliteten er færdiggjort, dog er der plads til optimereing og kommentering af kode

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import os
+import pandas as pd
 
 nations_file = "data/nations.txt"
-result_file = "data/result.txt"
+result_file = "data/results.csv"
 round_files = ["data/round1.txt", "data/round2.txt", "data/round3.txt"]
-results_list = [0, 0, 0, 0]
 
 
 def get_nations():
     with open(nations_file, "r") as f:
         nation = f.read()
     nations = nation.rstrip().split("\n")
-    nations_dict = dict.fromkeys(nations, {})
-    print(nations_dict)
     return nations
 
 
-def cr_result_file(nations):
-    if os.path.isfile("data/result.txt"):
-        with open(result_file, "r") as f:
-            result = f.read()
-        results = result.split("\n")
-        for i, r in enumerate(results):
-            results_list[i] = int(r.split()[1])
+def get_scores():
+    if os.path.isfile(result_file):
+        df = pd.read_csv(result_file, index_col=0).sort_values("PTS", ascending=False)
     else:
-        _save_current_score_to_file(nations)
+        nations_dict = dict.fromkeys(get_nations(), {"M": 0, "W": 0, "D": 0, "L": 0, "G+": 0,
+                                                     "G-": 0, "GD": 0, "PTS": 0})
+        _save_current_score_to_file(nations_dict)
+        df = get_scores()
+
+    return df
 
 
-def _save_current_score_to_file(teams):
-    with open(result_file, "w") as f:
-        for i, team in enumerate(teams):
-            if len(teams) == i + 1:
-                f.write(f"{team} {games_won[i]} {results_list[i]}")
-            else:
-                f.write(f"{team} {results_list[i]}\n")
+def _save_current_score_to_file(df_dict):
+    df = pd.DataFrame.from_dict(df_dict, orient='index')
+    df.to_csv(result_file)
 
 
 def get_matches(match_files):
@@ -41,15 +36,11 @@ def get_matches(match_files):
         for match in match_files:
             with open(match, "r") as f:
                 round_matches = f.read()
-            round_matches = round_matches.rstrip().split("\n")
-            for r in round_matches:
-                all_matches.append(r)
+            all_matches.extend(round_matches.rstrip().split("\n"))
     elif isinstance(match_files, str):
         with open(match_files, "r") as f:
             round_matches = f.read()
-        round_matches = round_matches.rstrip().split("\n")
-        for r in round_matches:
-            all_matches.append(r)
+        all_matches.extend(round_matches.rstrip().split("\n"))
 
     return all_matches
 
@@ -57,9 +48,10 @@ def get_matches(match_files):
 def get_winner_teams(matches):
     winners = []
     for match in matches:
-        if _get_team_score(match, "s")[0] > _get_team_score(match, "s")[1]:
+        goals_scored = _get_team_score(match, "s")
+        if goals_scored[0] > goals_scored[1]:
             winners.append(_get_team_score(match, 't')[0])
-        elif _get_team_score(match, "s")[0] < _get_team_score(match, "s")[1]:
+        elif goals_scored[0] < goals_scored[1]:
             winners.append(_get_team_score(match, 't')[1])
         else:
             winners.append(_get_team_score(match, 't'))
@@ -79,24 +71,62 @@ def _get_team_score(match, team_score):
         return score
 
 
-def update_score(matches):
-    winners = get_winner_teams(get_matches(matches))
-    teams = get_nations()
-    for winner in winners:
-        if winner in teams:
-            for i in range(len(teams)):
-                if winner == teams[i]:
-                    results_list[i] += 3
-        else:
-            if isinstance(winner, list):
-                for r in winner:
-                    for i in range(len(teams)):
-                        if r == teams[i]:
-                            results_list[i] += 1
-    _save_current_score_to_file(teams)
+def update_scores(matches):
+    for match in matches:
+        competing_teams = match.split()[:3]
+        competing_teams.pop(1)
+        game_score = match.split()[-3:]
+        game_score.pop(1)
+        game_score = [int(s) for s in game_score]
+        for team in competing_teams:
+            if team in scores_dict:
+                scores_dict[team]["M"] += 1
+            if game_score[0] > game_score[1]:
+                if team == competing_teams[0]:
+                    scores_dict[team]["W"] += 1
+                    scores_dict[team]["G+"] += game_score[0]
+                    scores_dict[team]["G-"] += game_score[1]
+                    scores_dict[team]["GD"] += (game_score[0] - game_score[1])
+                    scores_dict[team]["PTS"] += 3
+                else:
+                    scores_dict[team]["L"] += 1
+                    scores_dict[team]["G+"] += game_score[1]
+                    scores_dict[team]["G-"] += game_score[0]
+                    scores_dict[team]["GD"] += (game_score[1] - game_score[0])
+                    scores_dict[team]["PTS"] += 0
+
+            elif game_score[0] < game_score[1]:
+                if team == competing_teams[1]:
+                    scores_dict[team]["W"] += 1
+                    scores_dict[team]["G+"] += game_score[1]
+                    scores_dict[team]["G-"] += game_score[0]
+                    scores_dict[team]["GD"] += (game_score[1] - game_score[0])
+                    scores_dict[team]["PTS"] += 3
+                else:
+                    scores_dict[team]["L"] += 1
+                    scores_dict[team]["G+"] += game_score[0]
+                    scores_dict[team]["G-"] += game_score[1]
+                    scores_dict[team]["GD"] += (game_score[0] - game_score[1])
+                    scores_dict[team]["PTS"] += 0
+            else:
+                if team == competing_teams[0]:
+                    scores_dict[team]["D"] += 1
+                    scores_dict[team]["G+"] += game_score[0]
+                    scores_dict[team]["G-"] += game_score[1]
+                    scores_dict[team]["GD"] += (game_score[0] - game_score[1])
+                    scores_dict[team]["PTS"] += 1
+                else:
+                    scores_dict[team]["D"] += 1
+                    scores_dict[team]["G+"] += game_score[1]
+                    scores_dict[team]["G-"] += game_score[0]
+                    scores_dict[team]["GD"] += (game_score[1] - game_score[0])
+                    scores_dict[team]["PTS"] += 1
+
+    _save_current_score_to_file(scores_dict)
 
 
-get_nations()
-#cr_result_file(get_nations())
-#update_score(round_files)
-
+current_scores = get_scores()
+print(f"Scores on Load:\n{current_scores}")
+scores_dict = current_scores.to_dict('index')
+update_scores(get_matches(round_files))
+print(f"Updated scores:\n{get_scores()}")
